2a64c8811133f023e890f2b8b77d4dabb0feeddc
amir <bashizade.official@gmail.com>
2023-08-05 18:06:33 +0330

    add controllers and reqest files

---
+++ app/Http/Controllers/Panel/EventController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers\Panel;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class EventController extends Controller
+{
+    public function index()
+    {
+
+    }
+
+    public function store()
+    {
+
+    }
+
+    public function update()
+    {
+
+    }
+
+    public function delete()
+    {
+
+    }
+
+}
