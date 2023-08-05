8d77ed634e88d7c91215aa5d491bf1fcfdcd2eb4
amir <bashizade.official@gmail.com>
2023-08-05 14:30:45 +0330

    add user migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,11 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->integer('admin_role')->default(0);
+            $table->integer('writer_role')->default(0);
+            $table->integer('acc_role')->default(0);
+            $table->integer('social_role')->default(0);
+            $table->integer('status')->default(1);
             $table->rememberToken();
             $table->timestamps();
         });
